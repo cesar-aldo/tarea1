2365060187d148811f7182663f3b95e53227de33
ALDOSX <aldosx@ALDOSXs-Mac.local>
2017-07-23 18:09:57 -0400

    primer commit

---
+++ rangoNros.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for var i = 0; i <= 100; i++ {
+    //print("# \(i) Bingo!!")
+        if i % 5 == 0 {
+            print("# \(i) Bingo!!!")
+        }
+        if i % 2 == 0 {
+            print("# \(i) par!!!")
+        }
+        if i % 2 != 0 {
+            print("# \(i) impar!!!")
+        }
+        if i >= 30 && i <= 40 {
+            print("# \(i) Viva Swift!!!")
+        }
+    }
